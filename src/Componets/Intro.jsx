d7800ba3b1a37b6cec6d388eb48f38b510b03f3f
MichaelKasingye <michaelkasingye@gmail.com>
2021-01-06 03:28:32 -0800

    add JSON stock data

--- src/Componets/Intro.jsx
+++ src/Componets/Intro.jsx
@@ -1,60 +1,79 @@
-import React from 'react'
-import '../Style/Intro.css'
-import {images} from '../Library/images';
+import React from "react";
+import "../Style/Intro.css";
+import { images } from "../Library/images";
 import Video from "../video/ad.mp4";
-import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
-import PhoneIphoneOutlinedIcon from '@material-ui/icons/PhoneIphoneOutlined';
-import TvOutlinedIcon from '@material-ui/icons/TvOutlined';
-import FitnessCenterOutlinedIcon from '@material-ui/icons/FitnessCenterOutlined';
-import ComputerOutlinedIcon from '@material-ui/icons/ComputerOutlined';
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import PhoneIphoneOutlinedIcon from "@material-ui/icons/PhoneIphoneOutlined";
+import TvOutlinedIcon from "@material-ui/icons/TvOutlined";
+import FitnessCenterOutlinedIcon from "@material-ui/icons/FitnessCenterOutlined";
+import ComputerOutlinedIcon from "@material-ui/icons/ComputerOutlined";
 
 function Intro() {
-    return (
-        <div className="intro">
-            <div className="categories">
-                <div className="category_items">
-                <StorefrontOutlinedIcon/><span>Supermarket</span>
-                </div>
-                <div className="category_items">
-                <PhoneIphoneOutlinedIcon/><span>Phone & Tablets</span>
-                </div>
-                <div className="category_items">
-                <TvOutlinedIcon/><span>Electronics</span>
-                </div>
-                <div className="category_items">
-                <FitnessCenterOutlinedIcon/><span>Sporting Goods</span>
-                </div>
-                <div className="category_items">
-                <ComputerOutlinedIcon/><span>Computing</span>
-                </div>
-            </div>
-            
-            <div className="showcase">
-           <span className="home_image" 
-        //    src={images.homeImage}
-            // alt="showcase"
-            /> 
-            </div>
-
-            <div className="right_section">
-                <div className="right_section_content">
-                    <div className="right_section_content_items">
-                    <img src={images.helpCenter} alt="Help center"/><span className="title">HELP CENTER<br/><span className="desc">Guide to customer Care</span></span>
-                    </div>
-                    <div className="right_section_content_items">
-                    <img src={images.easyReturn} alt="Easy Return"/><span className="title">EASY RETURN <br/><span className="desc">Quick Refund</span></span> 
-                    </div>
-                    <div className="right_section_content_items">
-                    <img src={images.sell} alt="Sell"/><span className="title">SELL<br/><span className="desc">Millions of Customers</span></span>
-                    </div>
-                </div>
-                <div className="right_section_ads">
-                    <video autostart autoPlay muted loop src={Video} alt="Ad"/>
-                </div>
-            </div>
-
+  return (
+    <div className="intro">
+      <div className="categories">
+        <div className="category_items">
+          <StorefrontOutlinedIcon />
+          <span>Supermarket</span>
         </div>
-    )
+        <div className="category_items">
+          <PhoneIphoneOutlinedIcon />
+          <span>Phone & Tablets</span>
+        </div>
+        <div className="category_items">
+          <TvOutlinedIcon />
+          <span>Electronics</span>
+        </div>
+        <div className="category_items">
+          <FitnessCenterOutlinedIcon />
+          <span>Sporting Goods</span>
+        </div>
+        <div className="category_items">
+          <ComputerOutlinedIcon />
+          <span>Computing</span>
+        </div>
+      </div>
+
+      <div className="showcase">
+        <span
+          className="home_image"
+          //    src={images.homeImage}
+          // alt="showcase"
+        />
+      </div>
+
+      <div className="right_section">
+        <div className="right_section_content">
+          <div className="right_section_content_items">
+            <img src={images.helpCenter} alt="Help center" />
+            <span className="title">
+              HELP CENTER
+              <br />
+              <span className="desc">Guide to customer Care</span>
+            </span>
+          </div>
+          <div className="right_section_content_items">
+            <img src={images.easyReturn} alt="Easy Return" />
+            <span className="title">
+              EASY RETURN <br />
+              <span className="desc">Quick Refund</span>
+            </span>
+          </div>
+          <div className="right_section_content_items">
+            <img src={images.sell} alt="Sell" />
+            <span className="title">
+              SELL
+              <br />
+              <span className="desc">Millions of Customers</span>
+            </span>
+          </div>
+        </div>
+        <div className="right_section_ads">
+          <video autostart autoPlay muted loop src={Video} alt="Ad" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Intro
+export default Intro;
